60fcc0027db568103844a549c78f8bf1c5f12019
Ryan <salvat36@gmail.com>
2023-03-02 14:50:30 -0600

    solved 15 tests

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(a) {
+    return (a += 1);
+}
+
+function decrement(a) {
+    return (a -= 1);
+}
+
+function makeInt(number) {
+    return parseInt(number, 10);
+}
+
+function preserveDecimal(number) {
+    return parseFloat(number, 10);
+}
